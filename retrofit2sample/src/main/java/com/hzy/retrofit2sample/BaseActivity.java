14bd492fae7c2ef93435c83de64dd234d9cec1d4
wxj <1292571922@qq.com>
2019-08-23 19:19:59 +0800

    修改baseactivity

--- retrofit2sample/src/main/java/com/hzy/retrofit2sample/BaseActivity.java
+++ retrofit2sample/src/main/java/com/hzy/retrofit2sample/BaseActivity.java
@@ -1,18 +1,13 @@
 package com.hzy.retrofit2sample;
 
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 import com.hzy.retrofit2sample.util.StatusBarUtil;
-
-import java.lang.reflect.Field;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
@@ -22,73 +17,23 @@ public abstract  class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(getLayoutId());
-        mUnbinder = ButterKnife.bind(this);
-        //initWindows();
+        setContentView(R.layout.activity_base);
         StatusBarUtil.setImmersiveStatusBar(this);
+        FrameLayout contentView = findViewById(R.id.contentView);
+        View view = View.inflate(this, getLayoutId(), null);
+        contentView.removeAllViews();
+        contentView.addView(view, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        mUnbinder = ButterKnife.bind(this);
         init();
     }
     public abstract void init();
     public abstract int getLayoutId();
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         if (mUnbinder != null && mUnbinder != Unbinder.EMPTY) {
             mUnbinder.unbind();
         }
-    }
-    private void initWindows() {
-        Window window = getWindow();
-        int color = getResources().getColor(android.R.color.transparent);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-            window.getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            //设置状态栏颜色
-            window.setStatusBarColor(color);
-            //设置导航栏颜色
-            window.setNavigationBarColor(color);
-            ViewGroup contentView = ((ViewGroup) findViewById(android.R.id.content));
-            View childAt = contentView.getChildAt(0);
-            if (childAt != null) {
-                childAt.setFitsSystemWindows(true);
-            }
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            //透明状态栏
-            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            //透明导航栏
-            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-            //设置contentview为fitsSystemWindows
-            ViewGroup contentView = (ViewGroup) findViewById(android.R.id.content);
-            View childAt = contentView.getChildAt(0);
-            if (childAt != null) {
-                childAt.setFitsSystemWindows(true);
-            }
-            //给statusbar着色
-            View view = new View(this);
-            view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, getStatusBarHeight(this)));
-            view.setBackgroundColor(color);
-            contentView.addView(view);
-        }
-    }
-    public static int getStatusBarHeight(Context context) {
-        Class<?> c = null;
-        Object obj = null;
-        Field field = null;
-        int x = 0;
-        int statusBarHeight = 0;
-        try {
-            c = Class.forName("com.android.internal.R$dimen");
-            obj = c.newInstance();
-            field = c.getField("status_bar_height");
-            x = Integer.parseInt(field.get(obj).toString());
-            statusBarHeight = context.getResources().getDimensionPixelSize(x);
-            return statusBarHeight;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return statusBarHeight;
     }
 }
