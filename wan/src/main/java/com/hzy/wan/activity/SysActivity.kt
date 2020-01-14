640c1ee6a4a861a4f85c2b4d12fecfce3d5184a7
wxj <1292571922@qq.com>
2020-01-14 18:37:56 +0800

    wan

---
+++ wan/src/main/java/com/hzy/wan/activity/SysActivity.kt
@@ -0,0 +1,12 @@
+package com.hzy.wan.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SysActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sys)
+    }
+}
